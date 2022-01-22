2f564246419d707e22d049d18396b44de9139cf4
unishako <tnishida0822@icloud.com>
2022-01-22 12:51:38 +0900

    モックを使ったテストの検証

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -7,20 +7,21 @@ namespace Test;
 public class UnitTest1
 {
     [Fact]
-    public void 加算テスト1()
+    public void モック無テスト()
     {
         IHomeService service = new HomeService(new HomeRepository());
         Assert.Equal(103, service.Add(1, 2));        
     }
     
-    [Fact]
-    public void 加算テスト2()
+    [Theory]
+    [InlineData(258, 1, 2)]
+    [InlineData(260, 2, 3)]
+    public void モック有テスト(int expected, int x, int y)
     {
         var mock = new Mock<IHomeRepository>();
         mock.Setup(r => r.Count()).Returns(255);
 
         IHomeService service = new HomeService(mock.Object);
-        Assert.NotEqual(103, service.Add(1, 2));
-        Assert.Equal(258, service.Add(1, 2));
+        Assert.Equal(expected, service.Add(x, y));
     }
 }
